b05c8d2ff3d53f51a53ee6f076fce63248bed509
AhmedMagdyAbdAlhady <140403024+AhmedMagdyAbdAlhady@users.noreply.github.com>
2024-12-27 04:55:29 +0200

    update file weather

--- assets/js/main.js
+++ assets/js/main.js
@@ -140,7 +140,7 @@ document.addEventListener("DOMContentLoaded", () => {
   // },
     {
         id: "1",
-        image: "assets/webSits/weatherApp/w.png",
+        image: "assets/webSits/weather.png",
         category: "website",
         title: "API using HTML,Css,JS",
     },
